d8343c0d24fc7c695c887cd17254b40c1df15308
Fatma Elzahraa Atef <88749492+fatma8720@users.noreply.github.com>
2024-07-23 22:57:23 +0300

    Add  Controller and Route for journal

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShipmentController;
+use App\Http\Controllers\JournalController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('shipments', ShipmentController::class);
+Route::post('shipments/{shipment}/status', [ShipmentController::class, 'changeStatus'])->name('shipments.changeStatus');
+
+Route::resource('journals', JournalController::class)->only(['index']);
